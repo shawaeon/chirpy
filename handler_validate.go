0898a7b8310b4b2408ddc6bc3acc83e4b54fedff
shawaeon <jens.sjogr@gmail.com>
2025-02-24 09:15:54 +0200

    Add simple profanity filter

--- handler_validate.go
+++ handler_validate.go
@@ -3,6 +3,8 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"slices"
+	"strings"
 )
 
 // Chirp length validation
@@ -14,7 +16,7 @@ func handlerChirpsValidate(w http.ResponseWriter, r *http.Request) {
 		Body string `json:"body"`
 	}
 	type validResponse struct {
-		Valid bool `json:"valid"`
+		CleanedBody string `json:"cleaned_body"`
 	}
 
 	params := parameters{}
@@ -28,8 +30,25 @@ func handlerChirpsValidate(w http.ResponseWriter, r *http.Request) {
 		respondWithError(w, http.StatusBadRequest, "Chirp is too long", nil)		
 		return
 	}	
-	
+	msg := profanityFilter(params.Body)	
 	respondWithJSON(w, http.StatusOK, validResponse{
-		Valid: true,
+		CleanedBody: msg,
 	})
+}
+
+func profanityFilter(msg string) string {	
+	profanities := []string{
+		"kerfuffle",
+		"sharbert",
+		"fornax",
+	}
+	
+	words := strings.Fields(msg)
+	for i, word := range words {
+		if slices.Contains(profanities, strings.ToLower(word)){
+			words[i] = "****"
+		}
+	}
+	msg = strings.Join(words, " ")
+	return msg
 }
